0714d927941c7c0cb77707bd89d3683f066b90b8
wagigi <wagigi@users.noreply.github.com>
2017-02-24 12:05:00 +0100

    fix md5 def. Now python3 compliant
    New readme
    
    Update README.rst
    
    Revert "Update README.rst"
    
    This reverts commit 891c96f0de27a2ee9da9e72d0f7b002dfc62136a.
    
    Revert "New readme"
    
    This reverts commit 941ab03f0bc471533a0a09436907c9b5a7e89369.

--- fabtools/files.py
+++ fabtools/files.py
@@ -166,25 +166,25 @@ def md5sum(filename, use_sudo=False):
                   warn_only=True):
         # Linux (LSB)
         if exists(u'/usr/bin/md5sum'):
-            res = func(u'/usr/bin/md5sum %(filename)s' % str(locals()))
+            res = func(u'/usr/bin/md5sum %(filename)s' % locals())
         # BSD / OS X
         elif exists(u'/sbin/md5'):
-            res = func(u'/sbin/md5 -r %(filename)s' % str(locals()))
+            res = func(u'/sbin/md5 -r %(filename)s' % locals())
         # SmartOS Joyent build
         elif exists(u'/opt/local/gnu/bin/md5sum'):
-            res = func(u'/opt/local/gnu/bin/md5sum %(filename)s' % str(locals()))
+            res = func(u'/opt/local/gnu/bin/md5sum %(filename)s' % locals())
         # SmartOS Joyent build
         # (the former doesn't exist, at least on joyent_20130222T000747Z)
         elif exists(u'/opt/local/bin/md5sum'):
-            res = func(u'/opt/local/bin/md5sum %(filename)s' % str(locals()))
+            res = func(u'/opt/local/bin/md5sum %(filename)s' % locals())
         # Try to find ``md5sum`` or ``md5`` on ``$PATH`` or abort
         else:
             md5sum = func(u'which md5sum')
             md5 = func(u'which md5')
             if exists(md5sum):
-                res = func('%(md5sum)s %(filename)s' % str(locals()))
+                res = func('%(md5sum)s %(filename)s' % locals())
             elif exists(md5):
-                res = func('%(md5)s %(filename)s' % str(locals()))
+                res = func('%(md5)s %(filename)s' % locals())
             else:
                 abort('No MD5 utility was found on this system.')
 
